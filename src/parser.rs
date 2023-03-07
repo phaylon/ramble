c2c0c811bc3b9a0ead09f6b2fb14a81ff76f4283
Robert Sedlacek <rs@474.at>
2023-03-08 00:39:52 +0100

    error message text

--- src/parser.rs
+++ src/parser.rs
@@ -184,7 +184,7 @@ impl ParseError {
 
 impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} at {}", self.fmt_message(), self.fmt_location())
+        write!(f, "{} on {}", self.fmt_message(), self.fmt_location())
     }
 }
 
